3bbcdf710e95cab356c4f41713b156ac2504eb03
Kevin Grant <ConsciousK@yahoo.com>
2019-08-29 18:29:41 -0400

    Completed MVP. Pulling in NASA data via api key and rendering via Hooks and State Props.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PhotoCard from './components/PhotoCard';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    console.log("first render")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=jad07zoq0RscmTZa5rSyscaha1LUNUHLm3ztewE0")
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun !
       </p>
+      <PhotoCard title={data.title} 
+                  url={data.url}
+                  explanation={data.explanation}/>
     </div>
   );
 }
